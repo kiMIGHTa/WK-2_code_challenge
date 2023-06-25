279ed55477167b12ce4256c6ae1beb2c1040269e
Dennis Kimaita <dkimaita22@gmail.com>
2023-06-25 11:01:48 +0300

    dynamically implement an image to our page

--- main.js
+++ main.js
@@ -2,19 +2,31 @@ fetch('http://localhost:3000/characters')
 .then((response)=>response.json())
 .then((json)=>renderCharacters(json))
 
-function renderCharacters(characters){
-    console.log(characters)
-    const main = document.querySelector('div')
-    characters.forEach(character=>{
-        const p=document.createElement('p')
-        p.innerHTML=`${character.id} ${character.name}`
-        main.appendChild(p)
-    })
-    // console.log(characters.name)
+// function renderCharacters(characters){
+//     console.log(characters)
+//     const main = document.querySelector('div')
+//     characters.forEach(character=>{
+//         const p=document.createElement('p')
+//         p.innerHTML=`${character.id} ${character.name}`
+//         main.appendChild(p)
+//     })
+//     // console.log(characters.name)
 
+
+// }
+
+function post(){
+    //create an image element for our cute animals
+    let content=document.querySelector('div')
+    let divContent=document.createElement('div')
+
+    let img=document.createElement('img')
+    img.src="https://thumbs.gfycat.com/EquatorialIckyCat-max-1mb.gif"
+    divContent.appendChild(img)
+    content.appendChild(divContent)
 
 }
 
 document.addEventListener('DOMContentLoaded', function(){
-    renderCharacters()
+    post()
 })
