4000cc657176fba738d1eef03e651d7eb4968648
Dennis Kimaita <dkimaita22@gmail.com>
2023-06-25 19:46:08 +0300

    add a comment section

--- main.js
+++ main.js
@@ -49,20 +49,86 @@ function post(characters){
 
 
     }
-
-    // votes++
-    // btn.innerHTML = 'Liked!'
-    // // btn.disabled = true
-
+    
     console.log(`${characterName} LikeCount: ${votes}`)
-    characters.forEach((character)=> console.log(character) )
+    characters.forEach((character)=> console.log(character) ) 
+     })
 
-   })
 
-   
-        
-content.appendChild(divContent)
-})
+
+     let commentSection=document.createElement('div')
+     commentSection.className = 'commentSection'
+    // Create the form element
+    const form = document.createElement('form')
+    form.id = `${character.id}'s comment section `
+    form.action = ''
+    form.method = 'POST'
+
+
+
+    // Create the input element
+    const input = document.createElement('input')
+    input.type = 'text'
+    input.id = 'new-comment'
+    input.name = 'new-comment'
+    input.placeholder = 'Add your comment...'
+
+    form.addEventListener('submit', (event)=>{
+        event.preventDefault()
+        addComment(document.getElementById('new-comment').value)
+        form.reset()
+      })
+
+
+    form.appendChild(input)
+    commentSection.appendChild(form)
+    // form.appendChild(submitInput);
+
+    // Create the list container
+    const commentContainer = document.createElement('div')
+    commentContainer.id = 'comments'
+
+    // Create the heading element
+    const heading = document.createElement('h2')
+    heading.textContent = 'Comments'
+
+    // Create the unordered list
+    let commentList = document.createElement('ul')
+    commentList.id = 'individualComment'
+
+    // Append the heading and task list to the list container
+    commentContainer.appendChild(heading)
+    commentContainer.appendChild(commentList)
+
+    //
+    commentSection.appendChild(commentContainer)
+
+
+    // Append the form and list container to the app container
+    // const commentSection = document.createElement('div')
+    // commentSection.id = 'commentSection'
+
+
+
+    function addComment(comment){
+        let item = document.createElement('li')
+        item.textContent=`${comment}  `
+      
+        let btn = document.createElement('button')
+        btn.textContent = 'x'
+        btn.addEventListener('click', deletesComment)
+      
+        item.appendChild(btn)
+        commentList.appendChild(item)
+      
+      }
+      function deletesComment(event){
+        event.target.parentNode.remove()
+      
+      }
+     content.appendChild(divContent)
+     content.appendChild(commentSection)
+    })
 
 }
 
@@ -72,4 +138,4 @@ content.appendChild(divContent)
 
 // document.addEventListener('DOMContentLoaded', function(){
 //     renderCharacters()
-// }
+// })
